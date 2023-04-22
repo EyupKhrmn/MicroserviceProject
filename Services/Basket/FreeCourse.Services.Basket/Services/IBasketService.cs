04605a781a3deb2ea424189e9e66b17f73c30d09
Eyüp Kahraman <96554015+EyupKhrmn@users.noreply.github.com>
2023-04-22 19:19:11 +0300

    Basket Service kodlandı içerisi dolduruldu.

---
+++ Services/Basket/FreeCourse.Services.Basket/Services/IBasketService.cs
@@ -0,0 +1,13 @@
+﻿using CourseServiceCatalog.Shares;
+using FreeCourse.Services.Basket.Dtos;
+
+namespace FreeCourse.Services.Basket.Services;
+
+public interface IBasketService
+{
+    Task<ResponseDto<BasketDto>> GetBaskete(string userId);
+
+    Task<ResponseDto<bool>> SaveOrUpdate(BasketDto basketDto);
+
+    Task<ResponseDto<bool>> DeleteBasket(string userId);
+}
