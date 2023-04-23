ab0528134b3ddf4cd01e27e785052a4b924737f3
Eyüp Kahraman <96554015+EyupKhrmn@users.noreply.github.com>
2023-04-24 00:34:57 +0300

    Discount.cs projesi eklendi, IDiscountService.cs oluşturuldu DiscountService oluşturulmadı.

---
+++ Services/Discount/FreeCourse.Services.Discount/Services/IDiscountService.cs
@@ -0,0 +1,18 @@
+﻿using CourseServiceCatalog.Shares;
+
+namespace FreeCourse.Services.Discount.Services;
+
+public interface IDiscountService
+{
+    Task<ResponseDto<List<Entities.Discount>>> GetAll();
+
+    Task<ResponseDto<Entities.Discount>> GetById(int id);
+
+    Task<ResponseDto<NoContent>> Save(Entities.Discount discount);
+
+    Task<ResponseDto<NoContent>> Update(Entities.Discount discount);
+
+    Task<ResponseDto<NoContent>> Delete(int id);
+
+    Task<ResponseDto<Entities.Discount>> GetByCodeAndUserId(string code, string userId);
+}
