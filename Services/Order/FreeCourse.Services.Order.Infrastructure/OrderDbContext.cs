f80dcecd622d006fc87c8765a0512e1a634e7006
Eyüp Kahraman <96554015+EyupKhrmn@users.noreply.github.com>
2023-05-01 12:07:59 +0300

    Order.cs & OrderItem.cs & OrderDbContext.cs eklendi bu sınıflar birbirleri ile ilişkilendirildi. Gerekli paketler yüklendi.

---
+++ Services/Order/FreeCourse.Services.Order.Infrastructure/OrderDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.Intrinsics.X86;
+using FreeCourse.Services.Order.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+
+namespace FreeCourse.Services.Order.Infrastructure;
+
+public class OrderDbContext : DbContext
+{
+    public const string DEFAULT_SHEMA = "ordering";
+
+    public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<Domain.Model.Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Domain.Model.Order>().ToTable("Orders", DEFAULT_SHEMA);
+        modelBuilder.Entity<OrderItem>().ToTable("Orders", DEFAULT_SHEMA);
+        modelBuilder.Entity<OrderItem>().Property(x => x.Price).HasColumnType("decimal(18,2)");
+
+        modelBuilder.Entity<Domain.Model.Order>().OwnsOne(x => x.Address).WithOwner();
+        base.OnModelCreating(modelBuilder);
+    }
+}
