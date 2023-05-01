5ecf2c6fff2d93eb71d9be3362ccc178b8e5a68b
Eyüp Kahraman <96554015+EyupKhrmn@users.noreply.github.com>
2023-05-01 12:21:51 +0300

    Dto sınıfları oluşturuldu. Auto mapper kütüphanesi kullanıldı ve CustomMap.cs sınıfı üzerinden işlemler yapıldı.

---
+++ Services/Order/FreeCourse.Services.Order.Application/Mapping/CustomMap.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using FreeCourse.Services.Order.Application.Dtos;
+using FreeCourse.Services.Order.Domain.Model;
+
+namespace FreeCourse.Services.Order.Application.Mapping;
+
+public class CustomMap : Profile
+{
+    public CustomMap()
+    {
+        CreateMap<Domain.Model.Order, OrderDto>().ReverseMap();
+        CreateMap<OrderItem, OrderItemDto>().ReverseMap();
+        CreateMap<Address, AddressDto>().ReverseMap();
+    }
+}
---
+++ Services/Order/FreeCourse.Services.Order.Application/Mapping/ObjectMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+
+namespace FreeCourse.Services.Order.Application.Mapping;
+
+public class ObjectMapper
+{
+    private static readonly Lazy<IMapper> Lazy = new Lazy<IMapper>(() =>
+    {
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<CustomMap>();
+        });
+
+        return config.CreateMapper();
+    });
+
+    public static IMapper Mapper => Lazy.Value;
+}
